f7930ff3973441a9c970c4301240abf1d7d30be4
beadevil <86977130+beadevil@users.noreply.github.com>
2021-07-10 13:57:50 +0530

    Update devil.py

--- devil.py
+++ devil.py
@@ -4,6 +4,41 @@ from PIL import Image
 import base64
 from random import randrange
 
+
+import cv2
+from deepface import DeepFace
+
+faceCascade = cv2.CascadeClassifier( cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise IoError("Cannot open webcam !")
+
+while True:
+    ret,frame = cap.read()
+    
+    
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale( gray, 1.1, 4 )
+    
+    for( x,y,w,h ) in faces:
+        cv2.rectangle( frame , (x,y), (x+w, y+h), (0,255,0),2)
+    
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    
+    result = DeepFace.analyze(frame,actions=['emotion'])
+    cv2.putText(frame,result["dominant_emotion"],(50,50),font,3,(0,0,255),2,cv2.LINE_4)
+    
+    
+    cv2.imshow('Original video',frame)
+    
+    if cv2.waitKey(2) & 0xff == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
 # Page title
 st.write("""# Avatar Maker
 This app allows you to build your own custom Emoji.
