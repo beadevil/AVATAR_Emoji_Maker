0c0a40f2a8add803c104f7b375c5bc704a930f58
beadevil <86977130+beadevil@users.noreply.github.com>
2021-07-24 22:38:43 +0530

    Update devil.py

--- devil.py
+++ devil.py
@@ -5,7 +5,7 @@ import base64
 from random import randrange
 from keras.models import load_model
 from time import sleep
-from keras.preprocessing.image import img_to_array
+
 
 import cv2
 import numpy as np
